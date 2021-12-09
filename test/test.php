5537b1d87dcd7608f02ed8e399233e873e672b45
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-12-09 18:58:30 +0330

    read a xls file in test.php

--- test/test.php
+++ test/test.php
@@ -1,11 +1,30 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-$color = "red";
-echo "My car is " . $color . "<br>";
-?>
 
-</body>
-</html> 
+require '../vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Reader\Xls;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+
+$reader = new Xls();
+
+$spreadsheet = $reader->load("../raw/cities.xls");
+
+try {
+    $d = $spreadsheet->getSheet(0)->toArray();
+    echo count($d);
+    echo "<br>";
+    $sheetData = $spreadsheet->getActiveSheet()->toArray();
+    $i=1;
+    unset($sheetData[0]);
+    foreach ($sheetData as $t) {
+        // process element here;
+        // access column by index
+        echo $i."---".$t[0].",".$t[1]." <br>";
+        $i++;
+    }
+
+} catch (\PhpOffice\PhpSpreadsheet\Exception $e) {
+    echo "error";
+}
+
+
