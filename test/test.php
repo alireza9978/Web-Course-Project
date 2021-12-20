1f132097747dc8dc645b01b92999ae1f6a57cef4
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-12-20 11:55:09 +0330

    load png image draw circle in the center of loaded image save final image show image in output

--- test/test.php
+++ test/test.php
@@ -1,23 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="test.css">
+
+</head>
+<body>
 <?php
+$circle_diameter = 100;
+$main_image_width = 2560;
+$main_image_height = 2560;
 
-require '../vendor/autoload.php';
-
-use PhpOffice\PhpSpreadsheet\Reader\Xls;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-
-$reader = new Xls();
-
-$spreadsheet = $reader->load("../raw/states.xls");
-
-try {
-    $target_data = [];
-    $sheetData = $spreadsheet->getSheet(0)->toArray();
-    foreach ($sheetData as $t) {
-        $target_data[$t[0]] = $t[1];
-    }
-    print_r($target_data);
-} catch (\PhpOffice\PhpSpreadsheet\Exception $e) {
-    echo "error";
-}
-
-
+$image_path = "map.png";
+$output_path = "out.png";
+$im = imagecreatefrompng($image_path);
+// Create a colour.
+$white = imagecolorallocate($im, 255, 0, 0);
+// Draw a cirlce in the middle of the image.
+imagefilledellipse($im, $main_image_height / 2, $main_image_height / 2, $circle_diameter, $circle_diameter, $white);
+// Save the image to a file.
+// output the picture
+imagepng($im, $output_path);
+// Destroy the image handler.
+imagedestroy($im);
+?>
+<img src="<?php echo $output_path ?>" alt="iran map">
+</body>
+</html>
