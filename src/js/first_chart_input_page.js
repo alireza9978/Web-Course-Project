d2e7ededf1633c26fbd4d9c7744b957b81894800
Hadi Tamimi <hadi1376tm@gmail,com>
2022-01-03 12:28:35 +0330

    iran map with states names

--- src/js/first_chart_input_page.js
+++ src/js/first_chart_input_page.js
@@ -1,8 +1,35 @@
-function dynamic_input(count, element_id) {
-    const element = document.getElementById(element_id);
-    element.onchange(function () {
-        element.style = ""
-    });
-    const markup = "<tr><td><label for='memName'>Enter name: </label></td><td><input type='text' name='memName' id='memName'></td><td></td><td></td></tr>";
+function chartTypeSelectFunction() {
+    const x = document.getElementById("chart_type").value;
+    const y = document.getElementById("sum_up");
+    const z = document.getElementById("sum_up_txt");
+    if (x === "1") {
+        document.getElementById('chart_data').value = '{"17":10,"19":20}';
+        document.getElementById('output_path').value = '/home/alireza/Desktop';
+        document.getElementById('output_name').value = 'states.png';
+        if (y.style.display === "block") {
+            y.style.display = "none";
+            z.style.display = "none";
+        }
 
+    }
+    if (x === "2") {
+        document.getElementById('chart_data').value = '{"33":5,"427":5, "415":5, "369":7}';
+        document.getElementById('output_path').value = '/home/alireza/Desktop';
+        document.getElementById('output_name').value = 'cities.png';
+
+        if (y.style.display === "none") {
+            y.style.display = "block";
+            z.style.display = "block";
+        }
+
+    }
+    if (x === "3") {
+        document.getElementById('chart_data').value = '{"34":10,"108":20, "56":15}';
+        document.getElementById('output_path').value = '/home/alireza/Desktop';
+        document.getElementById('output_name').value = 'world.png';
+        if (y.style.display === "block") {
+            y.style.display = "none";
+            z.style.display = "none";
+        }
+    }
 }
