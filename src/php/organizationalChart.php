42383d153336cfda3a61873580152354acfe4fa3
AmirES1998 <amir.malek.esfandiari@gmail.com>
2022-01-04 16:50:06 +0330

    task 3 add phpToOrgChart create Chart from array

--- src/php/organizationalChart.php
+++ src/php/organizationalChart.php
@@ -1,7 +1,17 @@
+<!DOCTYPE >
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link type="text/css" rel="stylesheet" href="../css/style.css"/>
+</head>
+<body>
+
+
+
 <?php
-
+//demo
 require '../../vendor/autoload.php';
-
+include './PHPtoOrgChart.php';
 use JsonSchema\Validator;
 use JsonSchema\Constraints\Constraint;
 
@@ -25,10 +35,41 @@ function jsonValidator($data): bool
 }
 
 
-$data = json_decode(file_get_contents('php://input'));
 
-if (jsonValidator($data)) {
-    var_dump($data);
-} else {
-    echo " \n\r  eeeeeerrrrrrorrrrrr";
-}
+$users = json_decode(file_get_contents('php://input'));
+
+//if (jsonValidator($users)) {
+//
+//} else {
+//    echo " \n\r  eeeeeerrrrrrorrrrrr";
+//}
+
+
+$data=array(
+    'a'=>array(
+//            'postId' => 0 ,
+
+        'aa'=>array(
+            'aaa'=>'Mike',
+            'aab'=>'Look',
+            'aac'=>'Rum',
+        ),
+        'bb'=>array(
+            'aaa'=>'123',
+            'aab'=>'567',
+            'aac'=>'890',
+            'bbdd'=>array(
+                'aaa'=>'123',
+                'aab'=>'567',
+                'aac'=>'890',
+            ),
+        ),
+    )
+);
+
+echo '<div class="orgchart ">';
+PHPtoOrgChart($data);
+echo '</div>';
+?>
+</body>
+</html>
